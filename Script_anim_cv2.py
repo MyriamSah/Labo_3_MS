1b9becc4207c6f5a098b9c0e15dfd1ed89e00b47
MyriamSah <85957429+MyriamSah@users.noreply.github.com>
2021-08-02 17:37:28 -0400

    Update Script_anim_cv2.py

--- Script_anim_cv2.py
+++ Script_anim_cv2.py
@@ -23,8 +23,6 @@ X2 = [60,60,250,450,450]
 Y2 = [60,310,190,60,310]
 
 fig= plt.figure()
-lines = plt.plot([], color = "orange", linewidth = 5)
-line = lines[0]
 dots = plt.plot(X2,Y2, "bo", markersize=30)
 plt.axis("scaled")
 plt.xlim(0,700)
@@ -38,21 +36,32 @@ img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR) # Convert color to opencv's default BG
 
 frame = img
 height, width, layers = frame.shape
-video = cv2.VideoWriter(r'C:/Users/Myriam/Desktop/Université_Myriam/8esession/PSY3009/Anim_Nouv_1.mp4', 0, 100, (width,height))
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+video = cv2.VideoWriter(r'C:/Users/Myriam/Desktop/Université_Myriam/8esession/PSY3009/Anim_Nouv_2.mp4', fourcc, 100, (width,height))
 
 x_data = []
 y_data = []
 
 for i in range(30000):
-    x_data.append(X[i])
-    y_data.append(Y[i])
-    if x_data[-1] == 0 and y_data[-1] == 0:
-        x_data.clear()
-        y_data.clear()
-    fig.canvas.draw() # Draw the image on the canvas (i.e. apply the functions above), without displaying it
-    img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='') # Not sure how, but it sends the pixel values of the canvas to an array (called img)
-    img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,)) # Reshape the array so it's 3d (height X width X layer(color))
-    img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+        x_data.append(X[i])
+        y_data.append(Y[i])
+        if x_data[-1] == 0 and y_data[-1] == 0:
+            x_data.clear()
+            y_data.clear()
+        fig= plt.figure()
+        lines = plt.plot(x_data, y_data, color = "orange", linewidth = 5)
+        line = lines[0]
+        dots = plt.plot(X2,Y2, "bo", markersize=30)
+        plt.axis("scaled")
+        plt.xlim(0,700)
+        plt.ylim(0,500)
     
 
+        fig.canvas.draw() # Draw the image on the canvas (i.e. apply the functions above), without displaying it
+        img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='') # Not sure how, but it sends the pixel values of the canvas to an array (called img)
+        img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,)) # Reshape the array so it's 3d (height X width X layer(color))
+        img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+        
+        video.write(img)    
+cv2.destroyAllWindows()
 video.release()
